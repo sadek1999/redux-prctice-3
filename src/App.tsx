550896628d0d636b31524ba6b1119435f4e57ab3
sadek1999 <sadekali1530@gmail.com>
2024-08-13 22:08:20 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+
+
+  return (
+    <>
+      <h1>My ToDo </h1>
+      <Button>Click Me</Button>
+    </>
+  )
+}
+
+export default App
